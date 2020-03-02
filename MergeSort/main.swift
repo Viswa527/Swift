d8e2363d1b17ea2fc9b2b832c44c4b82a98fd3cf
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 14:47:20 +0530

    [Viswa] comments are added to Merge Sort program

--- MergeSort/main.swift
+++ MergeSort/main.swift
@@ -15,9 +15,10 @@ func mergeSort(_ array:Array<Int>) -> [Int]
   if array.count > 1
     {
         let halfLen = array.count / 2
-        let leftArr = mergeSort(Array(array[0 ..< halfLen]))
+        let leftArr = mergeSort(Array(array[0 ..< halfLen]))                    // divides the Array into half and pass to mergesort method until the length of array comes to 1
         let rightArr = mergeSort(Array(array[halfLen ..< array.count]))
-        return merge(leftArr, rightArr)
+        
+        return merge(leftArr, rightArr)                                         // while returning all the arrays are merged in sorted manner
         
   }
     else
@@ -27,13 +28,13 @@ func mergeSort(_ array:Array<Int>) -> [Int]
     }
 }
 
-func merge(_ left:Array<Int>, _ right:Array<Int>) -> [Int]
+func merge(_ left:Array<Int>, _ right:Array<Int>) -> [Int]                      // mergeing mechanism
 {
   var leftIndex = 0
   var rightIndex = 0
-
   var orderedArray:Array<Int>=[]
-  while leftIndex < left.count && rightIndex < right.count
+    
+  while leftIndex < left.count && rightIndex < right.count                      //sorts the two array up until one array length comes to nil or 0
   {
     let leftElement = left[leftIndex]
     let rightElement = right[rightIndex]
@@ -57,23 +58,19 @@ func merge(_ left:Array<Int>, _ right:Array<Int>) -> [Int]
             rightIndex += 1
         }
     }
+ }
+  while leftIndex < left.count                                                     // the elements which are not insert,will be inserted here
+    {
+        orderedArray.append(left[leftIndex])
+        leftIndex += 1
+    }
+  while rightIndex < right.count                                                  // same as above comments if exists in  right array added here
+    {
+        orderedArray.append(right[rightIndex])
+        rightIndex += 1
+    }
+    return orderedArray
 }
-
-  while leftIndex < left.count
-  {
-    orderedArray.append(left[leftIndex])
-    leftIndex += 1
-  }
-
-  while rightIndex < right.count
-  {
-    orderedArray.append(right[rightIndex])
-    rightIndex += 1
-  }
-  
-  return orderedArray
-}
-
 var sortedArray = mergeSort(unSortedArray)
 for i in sortedArray
 {
