cbf96c07bde4575b95855c8f53c022b67463be63
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 12:08:39 +0530

    [Viswa] comments are added to the InsertionSort program

--- InsertionSort/main.swift
+++ InsertionSort/main.swift
@@ -8,25 +8,25 @@
 
 import Foundation
 
-var unSortedArray = [6,8,3,12,5,14,2,44,1,9,4,7,0]
-func insertionSort(_ array:Array<Int>) -> [Int]
+var unSortedArray = [6,8,3,12,5,14,2,44,1,9,4,7,0]      // targeted array to make into sorted Array
+func insertionSort(_ array:Array<Int>) -> [Int]         // Insertion sort function returns sorted Array
 {
-    var arrSort = array
+    var arrSort = array                                 // taken into new Array because array is umutable so we copied into arrSort
     for i in 1...array.count-1
     {
-        let preV = arrSort[i]
+        let value = arrSort[i]                          // to insert the value in postion
         var index = i-1
-        while index >= 0 && preV < arrSort[index]
+        while index >= 0 && value < arrSort[index]      // condition index should me greater then are equal to zero and values must be compared
         {
-            let temp = arrSort[index+1]
+            let temp = arrSort[index+1]                 // swapping two values in Array
             arrSort[index+1] = arrSort[index]
             arrSort[index] = temp
             index=index-1
         }
     }
-    return arrSort
+    return arrSort                                      // returning the Array
 }
-var sortedArray = insertionSort(unSortedArray)
+var sortedArray = insertionSort(unSortedArray)          // collecting the Array which is returned by the insertionSort function
 for i in sortedArray
 {
     print(i,terminator:" ")
