fd8b847476f73f95e681626192a246b265a5aa26
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 15:23:11 +0530

    [Viswa] modified s3 DayofWeek program to Algorithms

--- DayOfWeek/main.swift
+++ DayOfWeek/main.swift
@@ -11,6 +11,5 @@ var name=["Sunday","Monday","Tuesday","Wenesday","Thursday","Friday","Saturday"]
 var day =  2
 var month = 3
 var year = 2020
-year = year-(14-month)/12
-day = (day+(year+(year/4)+(year/400)-(year/100))+(31*(month + 12 * ((14-month)/12) - 2))/12) % 7
+day = (day+((year-(14-month)/12)+((year-(14-month)/12)/4)+((year-(14-month)/12)/400)-((year-(14-month)/12)/100))+(31*(month + 12 * ((14-month)/12) - 2))/12) % 7
 print(name[day])
