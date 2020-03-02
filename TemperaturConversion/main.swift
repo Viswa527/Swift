9c466cd1853883fda569feaef8fb6ec3603a274b
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 16:54:25 +0530

    [Viswa] Comments & ReadLines added to Temperature program in Algorithms

--- TemperaturConversion/main.swift
+++ TemperaturConversion/main.swift
@@ -10,24 +10,24 @@ import Foundation
 
 print("Enter c or C for Celsius and f or F for Fahrenheit  : ",terminator:" ")
 var charact = readLine()!
-func celsisus(_ values:Double) -> Double
+func celsisus(_ values:Double) -> Double            // function to convert farhenheit to celsisus
 {
     return values * (9/5) + 32
 }
-func fahrenheit(_ value:Double) -> Double
+func fahrenheit(_ value:Double) -> Double           // function to convert celsisus to farhenheit
 {
     return value - 32 * (5/9)
 }
 
 
-if charact == "c" || charact == "C"
+if charact == "c" || charact == "C"                             //check for "c" or "C"
 {
-    print("Enter the value in Farhen",terminator:" ")
+    print("Enter the value in Farhenheit",terminator:" ")
     print(celsisus(Double(readLine()!)!))
 }
 else
 {
-    if charact == "f" || charact == "F"
+    if charact == "f" || charact == "F"                           //checks for "f" or "F"
     {
         print("Enter the value in celsisus :",terminator:" ")
         print(fahrenheit(Double(readLine()!)!))
