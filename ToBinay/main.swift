6e18deab05c3abaaf349b6d037fb3faa3fc237f4
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 19:26:29 +0530

    [Viswa] ToBinary program is added to Algorithms

---
+++ ToBinay/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  ToBinay
+//
+//  Created by admin on 02/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+var number = 28
+var str = ""
+while number != 0
+{
+   let temp = number%2
+    str = String(temp)+str
+    number = number/2
+}
+if str.count >= 4 && str.count % 4 == 0
+{
+    print(str)
+}
+else
+{
+    let pad = 4 - str.count % 4
+    for _ in 0...pad-1
+    {
+        str = "0"+str
+    }
+    print(str)
+}
