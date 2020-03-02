4833ed1cefd5519cceef46265aa83a2da5561e9f
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 15:06:08 +0530

    [Viswa] modified vending machine program

--- Vending Machine/main.swift
+++ Vending Machine/main.swift
@@ -7,17 +7,17 @@
 //
 
 import Foundation
-var notes = [1000,500,100,50,10,5,2,1]
-var amount = 4965
-var i = 0
+var notes = [1000,500,100,50,10,5,2,1]          // by default Notes are defined
+var amount = 4965                               // Cash is entered
+var i = 0                                       // initialized to iterate through notes
 while amount != 0
 {
     if amount/notes[i] != 0
     {
         let noNotes = amount/notes[i]
-        let ok = notes[i]*noNotes
-        amount = amount-ok
-        print("\(notes[i]) * \(noNotes) = \(ok)")
+        let debited= notes[i]*noNotes
+        amount = amount-debited
+        print("\(notes[i]) * \(noNotes) = \(debited)")
     }
     i=i+1
 }
