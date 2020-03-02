eec38013a1284e58b93519355811dfabc70ab774
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 10:49:22 +0530

    [Viswa] added comments to the Anagram

--- Anagram/main.swift
+++ Anagram/main.swift
@@ -9,23 +9,23 @@ import Foundation
 var str = "heart"
 var str1 = "earth"
 
-str = str.replacingOccurrences(of: " ", with: "")
+str = str.replacingOccurrences(of: " ", with: "")               //to delete spaces or replaceing all spaces in String
 str1 = str1.replacingOccurrences(of: " ", with: "")
 
-str = str.uppercased()
+str = str.uppercased()                                          // to convert into uppercase
 str1 = str1.uppercased()
 
-var strArray=str.sorted()
+var strArray=str.sorted()                                       // to be sorted and returns an Array of characters
 var str1Array=str1.sorted()
 
-loop:if str1Array.count == strArray.count
+loop:if str1Array.count == strArray.count                  // nameing the condiitonal loop as "loop" checks both Arrays length are equal or not
 {
     for i in 0...str1Array.count-1
     {
         if str1Array[i] != strArray[i]
         {
             print("Not a Anagram")
-            break loop
+            break loop                                     // to exit the loop to end of "loop"
         }
     }
     print("Anagram")
