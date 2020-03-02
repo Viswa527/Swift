282a4b4497b8ee798d51b420284c90d7631cc98c
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 15:19:18 +0530

    [Viswa] added DayofWeek program to Algorithms

---
+++ DayOfWeek/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  DayOfWeek
+//
+//  Created by admin on 02/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+var name=["Sunday","Monday","Tuesday","Wenesday","Thursday","Friday","Saturday"]
+var day =  2
+var month = 3
+var year = 2020
+year = year-(14-month)/12
+var x = year+(year/4)+(year/400)-(year/100)
+month = month + 12 * ((14-month)/12) - 2
+day = (day+x+(31*month)/12) % 7
+print(name[day])
