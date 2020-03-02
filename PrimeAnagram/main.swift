f5fa09b1946b183d46702c11886d91a039173204
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 11:26:11 +0530

    [Viswa] added PrimeAnagram program

--- PrimeAnagram/main.swift
+++ PrimeAnagram/main.swift
@@ -7,6 +7,52 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var number = 300
+func isPrime(_ number:Int) ->Bool
+{
+    if number > 3 && number > -1
+    {
+        for i in 2...number-1
+        {
+            if number % i == 0
+            {
+                return false
+            }
+        }
+        return true
+    }
+    else
+    {
+        return true
+    }
+}
+func reverse(_ number:Int) -> Int
+{
+    var value = 0
+    var num=number
+    while (num != 0)
+    {
+        value = value * 10
+        value = value + num % 10
+        num = num / 10
+    }
+    return value
+}
+func isAnagram(_ number:Int) -> Bool
+{
+    if number == reverse(number)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+for i in 0...number
+{
+    if isPrime(i) && isAnagram(i)
+    {
+        print(i)
+    }
+}
