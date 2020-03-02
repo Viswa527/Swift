55aef820d7176d18539aa84df19a5d195dca8dab
Viswa527 <viswarupaachari88@gmail.com>
2020-03-02 12:52:42 +0530

    [Viswa] added Bubble Sort program

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -7,6 +7,27 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var unsortedArray = [8,27,2,5,7,48,1,10,15,3,4,6,9,0]
+func bubbleSort(_ array:Array<Int>) -> [Int]
+{
+    var arrSort:Array<Int> = array
+    let len=arrSort.count
+    for _ in 0...len-1
+    {
+        for j in 0...len-2
+        {
+            if arrSort[j+1] < arrSort[j]
+            {
+                let temp = arrSort[j+1]
+                arrSort[j+1] = arrSort[j]
+                arrSort[j] = temp
+            }
+        }
+    }
+    return arrSort
+}
+var sortedArray = bubbleSort(unsortedArray)
+for i in sortedArray
+{
+    print(i,terminator:" ")
+}
