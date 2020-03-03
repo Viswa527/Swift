b704b70b92230f07d45c3769d647b1d5ffef1262
Viswa527 <viswarupaachari88@gmail.com>
2020-03-03 12:45:57 +0530

    [Viswa] added.BinarySearch program in Algorithms

--- BinarySearch/main.swift
+++ BinarySearch/main.swift
@@ -8,13 +8,51 @@
 
 import Foundation
 
-var str = "hello world how are you what is going on"
-var find = "are"
-var strArr = str.split(separator: " ").sorted()
-for i in 0...strArr.count-1
+var str = "hello world how are you what omg is going on wha"
+var find = "hello"
+var strArr:Array<String> = []
+var t = ""
+for i in str
 {
-    if find == strArr[i]
+    if i == " "
     {
-        print(find)
+        strArr.append(t)
+        t = ""
+    }
+    else
+    {
+        t.append(i)
     }
 }
+strArr.append(t)
+strArr = strArr.sorted()
+func binarySearch(_ numbers: [String],_ value: String) -> Int
+{
+    var left = 0
+    var right = numbers.count - 1
+
+    while left <= right
+    {
+        let middle = (left + right)/2
+
+        if numbers[middle] < value
+        {
+            left = middle + 1
+        }
+        else
+        {
+            if numbers[middle] > value
+            {
+                right = middle - 1
+            }
+            else
+            {
+                return middle
+            }
+        }
+    }
+    return 0
+}
+var newChanges = binarySearch(strArr,find)
+print(newChanges)
+print(strArr)
