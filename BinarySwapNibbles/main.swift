efa8ff6277f2ea0b5ee18c75e314e1d01bae2055
Viswa527 <viswarupaachari88@gmail.com>
2020-03-03 14:26:26 +0530

    [Viswa] Refactor.class added to ToBinary program

---
+++ BinarySwapNibbles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BinarySwapNibbles
+//
+//  Created by admin on 03/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- ToBinay/main.swift
+++ ToBinay/main.swift
@@ -9,19 +9,23 @@
 import Foundation
 
 var number = Int(readLine()!)!                                  // to take input from the user
-func toBinary(_ number:Int) -> String                           // function to convert int -> Binary (String format)
+class Binary
 {
+    func toBinary(_ number:Int) -> String                           // function to convert int -> Binary (String format)
+    {
     var str = ""
     var numberI = number
-    while numberI != 0
-    {
-        let temp = numberI%2
-        str = String(temp)+str
-        numberI = numberI/2
+        while numberI != 0
+        {
+            let temp = numberI%2
+            str = String(temp)+str
+            numberI = numberI/2
+        }
+        return str
     }
-    return str
 }
-var binaryString = toBinary(number)
+var object = Binary()
+var binaryString = object.toBinary(number)
 if binaryString.count >= 4 && binaryString.count % 4 == 0           // checking the string bust be in length 4
 {
     print(binaryString)
