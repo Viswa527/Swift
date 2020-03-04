7194f65925b79113749041011dce769916685494
Viswa527 <viswarupaachari88@gmail.com>
2020-03-04 12:53:15 +0530

    [Viswa] Refactor.PrimeAnagram connected to Utility and function is called

--- PrimeAnagram/main.swift
+++ PrimeAnagram/main.swift
@@ -7,51 +7,10 @@
 //
 
 import Foundation
-var number = 1000                           // user input
-func isPrime(_ number:Int) ->Bool           // function to check prime number or not and returns Boolean value (true or False)
-{
-    if number > 3 && number > -1
-    {
-        for i in 2...number-1
-        {
-            if number % i == 0
-            {
-                return false
-            }
-        }
-        return true
-    }
-    else
-    {
-        return true
-    }
-}
-func reverse(_ number:Int) -> Int           // function to convert the number to reverse and returns the reversed number
-{
-    var value = 0
-    var num=number
-    while (num != 0)                        // Reverse mechanisam
-    {
-        value = value * 10
-        value = value + num % 10
-        num = num / 10
-    }
-    return value
-}
-func isAnagram(_ number:Int) -> Bool        // function to check the number is Anagram or not return Boolean value
-{
-    if number == reverse(number)
-    {
-        return true
-    }
-    else
-    {
-        return false
-    }
-}
+var number = 1000                         // user input
 for i in 0...number
 {
-    if isPrime(i) && isAnagram(i)           // checks Anagram and Prime then only prints value
+    if PrimeAnagram.isPrimeAnagram(i)           // checks Anagram and Prime then only prints value
     {
         print(i)
     }
--- Utility/PrimeAnagram.swift
+++ Utility/PrimeAnagram.swift
@@ -9,43 +9,51 @@
 import Foundation
 class PrimeAnagram
 {
-    public static func isPrimeAnagram()
+    public static func isPrimeAnagram(_ num:Int) -> Bool
     {
         
-    }
-    func isPrime(_ number:Int) ->Bool           // function to check prime number or not and returns Boolean value (true or False)
-    {
-        if number > 3 && number > -1
-        {
-            for i in 2...number-1
+        func isPrime(_ number:Int) ->Bool           // function to check prime number or not and returns Boolean value (true or False)
             {
-                if number % i == 0
+                if number > 3 && number > -1
+                {
+                    for i in 2...number-1
+                    {
+                        if number % i == 0
+                        {
+                            return false
+                        }
+                    }
+                    return true
+                }
+                else
+                {
+                    return true
+                }
+            }
+            func reverse(_ number:Int) -> Int           // function to convert the number to reverse and returns the reversed number
+            {
+                var value = 0
+                var num=number
+                while (num != 0)                        // Reverse mechanisam
+                {
+                    value = value * 10
+                    value = value + num % 10
+                    num = num / 10
+                }
+                return value
+            }
+            func isAnagram(_ number:Int) -> Bool        // function to check the number is Anagram or not return Boolean value
+            {
+                if number == reverse(number)
+                {
+                    return true
+                }
+                else
                 {
                     return false
                 }
             }
-            return true
-        }
-        else
-        {
-            return true
-        }
-    }
-    func reverse(_ number:Int) -> Int           // function to convert the number to reverse and returns the reversed number
-    {
-        var value = 0
-        var num=number
-        while (num != 0)                        // Reverse mechanisam
-        {
-            value = value * 10
-            value = value + num % 10
-            num = num / 10
-        }
-        return value
-    }
-    func isAnagram(_ number:Int) -> Bool        // function to check the number is Anagram or not return Boolean value
-    {
-        if number == reverse(number)
+        if isPrime(num) && isAnagram(num)
         {
             return true
         }
