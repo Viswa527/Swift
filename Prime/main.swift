5ad5459f3af0ebbf2c302358a4db1d608474df9a
Viswa527 <viswarupaachari88@gmail.com>
2020-03-04 12:47:32 +0530

    [Viswa] Refactor.Prime connected to Utility and function is called

--- Prime/main.swift
+++ Prime/main.swift
@@ -5,30 +5,12 @@
 //  Created by admin on 02/03/20.
 //  Copyright © 2020 admin. All rights reserved.
 //
-
 import Foundation
-var Number = 1000
-func isPrime(_ number:Int) ->Bool   // function to  find a prime  number or not
-{
-    if number > 3 && number > -1    // condition
-    {
-        for i in 2...number-1
-        {
-            if number % i == 0
-            {
-                return false
-            }
-        }
-        return true
-    }
-    else
-    {
-        return true
-    }
-}
+var Number = 32
+
 for i in 0...Number     // looping to all numbers
 {
-    if isPrime(i)       // checking prime or not through the isPrime() function
+    if Prime.isPrime(i)       // checking prime or not through the isPrime() function
     {
         print(i)        // prints PRIME numbers
     }
