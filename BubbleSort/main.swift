4e32b5d8190740462d20e1cf99e7fb5bbb1d6939
Viswa527 <viswarupaachari88@gmail.com>
2020-03-04 12:58:46 +0530

    [Viswa] Refactor.BubbleSort connected to Utility and function is called

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -16,27 +16,7 @@ for _ in 0...numberOfElements-1                         // reading the inputs fr
     let x = Int(readLine()!)!
     unsortedArray.append(x)
 }
-
-func bubbleSort(_ array:Array<Int>) -> [Int]            // function to bubble sort which returns sorted array
-{
-    var arrSort:Array<Int> = array
-    let len=arrSort.count
-    for _ in 0...len-2
-    {
-        for j in 0...len-2
-        {
-            if arrSort[j+1] < arrSort[j]                    //logic for bubble sort
-            {
-                let temp = arrSort[j+1]                     // swapping values in Array
-                arrSort[j+1] = arrSort[j]
-                arrSort[j] = temp
-            }
-        }
-    }
-    return arrSort
-}
-
-var sortedArray = bubbleSort(unsortedArray)
+var sortedArray = Bubble.bubbleSort(unsortedArray)
 
 for i in sortedArray
 {
