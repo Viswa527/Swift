5df3f8136c668699cefc033ca241d4e322a68ef8
Viswa527 <viswarupaachari88@gmail.com>
2020-03-04 12:57:09 +0530

    [Viswa] Refactor.InsertionSort connected to Utility and function is called

--- InsertionSort/main.swift
+++ InsertionSort/main.swift
@@ -14,25 +14,9 @@ for _ in 0...length-1
 {
     unsortedArray.append(Int(readLine()!)!)
 }                                                       // targeted array to make into sorted Array
-func insertionSort(_ array:Array<Int>) -> [Int]         // Insertion sort function returns sorted Array
-{
-    var arrSort = array                                 // taken into new Array because array is umutable so we copied into arrSort
-    for i in 1...array.count-1
-    {
-        let value = arrSort[i]                          // to insert the value in postion
-        var index = i-1
-        while index >= 0 && value < arrSort[index]      // condition index should me greater then are equal to zero and values must be compared
-        {
-            let temp = arrSort[index+1]                 // swapping two values in Array
-            arrSort[index+1] = arrSort[index]
-            arrSort[index] = temp
-            index=index-1
-        }
-    }
-    return arrSort                                      // returning the Array
-}
 
-var sortedArray = insertionSort(unsortedArray)
+var sortedArray = Insertion.insertionSort(unsortedArray)
+
 for i in 0...sortedArray.count-1
 {
     print(sortedArray[i],terminator:" ")
--- Utility/Insertion.swift
+++ Utility/Insertion.swift
@@ -7,23 +7,23 @@
 //
 
 import Foundation
-public class InsertionSort
+public class Insertion
 {
     public static func insertionSort(_ array:Array<Int>) -> [Int]         // Insertion sort function returns sorted Array
     {
-        var arrSort = array                                 // taken into new Array because array is umutable so we copied into arrSort
-        for i in 1...array.count-1
-        {
-            let value = arrSort[i]                          // to insert the value in postion
-            var index = i-1
-            while index >= 0 && value < arrSort[index]      // condition index should me greater then are equal to zero and values must be compared
+            var arrSort = array                                 // taken into new Array because array is umutable so we copied into arrSort
+            for i in 1...array.count-1
             {
-                let temp = arrSort[index+1]                 // swapping two values in Array
-                arrSort[index+1] = arrSort[index]
-                arrSort[index] = temp
-                index=index-1
+                let value = arrSort[i]                          // to insert the value in postion
+                var index = i-1
+                while index >= 0 && value < arrSort[index]      // condition index should me greater then are equal to zero and values must be compared
+                {
+                    let temp = arrSort[index+1]                 // swapping two values in Array
+                    arrSort[index+1] = arrSort[index]
+                    arrSort[index] = temp
+                    index=index-1
+                }
             }
-        }
-        return arrSort                                      // returning the Array
+            return arrSort                                      // returning the Array
     }
 }
