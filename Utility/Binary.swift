afef3b7aeda7217c410cdd7b4c44e162fdc8a8ce
Viswa527 <viswarupaachari88@gmail.com>
2020-03-04 12:28:08 +0530

    [Viswa] Refactor.added Binary function in utility

---
+++ Utility/Binary.swift
@@ -0,0 +1,24 @@
+//
+//  Binary.swift
+//  Utility
+//
+//  Created by admin on 04/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+public class Binary
+{
+    public static func toBinary(_ number : Int) -> String                           // function to convert int -> Binary (String format)
+    {
+        var str = ""
+        var numberI = number
+        while numberI != 0
+        {
+            let temp = numberI%2
+            str = String(temp)+str
+            numberI = numberI/2
+        }
+        return str
+    }
+}
