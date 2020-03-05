0e277dd6bb48b4579ff1834096867faf8696b638
Viswa527 <viswarupaachari88@gmail.com>
2020-03-05 10:55:20 +0530

    [Viswa] Added Factors of a number program

---
+++ Factors/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Factors
+//
+//  Created by admin on 05/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+var number = Int(readLine()!)!
+var array:[Int] = []
+
+for value in 1...number
+{
+    if number % value == 0
+    {
+        array.append(value)
+    }
+}
+print("Factors of the \(number) is  :  \(array)")
