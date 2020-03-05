17f52e51ff8d0d85fc3794986742d32d1220f877
Viswa527 <viswarupaachari88@gmail.com>
2020-03-05 10:25:18 +0530

    [Viswa] added FlipCoin program

---
+++ FlipCoin/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  FlipCoin
+//
+//  Created by admin on 05/03/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+var Number=Int(readLine()!)!
+var tails:Int=0
+var heads:Int=0
+for _ in 0...Number-1
+{
+    if Bool.random()
+    {
+        tails+=1
+    }
+    else
+    {
+        heads+=1
+    }
+}
+var percentageOfHead=(Float(heads)*100.0)/Float(Number)
+print("Percentage of getting Heads :  \(percentageOfHead)")
+print("Percentage of getting Tails :  \(100-percentageOfHead)")
