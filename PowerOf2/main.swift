77cbd1a74818c0ef17c26128f037e2867c4f47c3
Viswa527 <viswarupaachari88@gmail.com>
2020-03-05 10:41:31 +0530

    [Viswa] Refactor. added comments to PowerOf2

--- PowerOf2/main.swift
+++ PowerOf2/main.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 var number = Int(readLine()!)!
-var value = 1
+var value = 1                    // Intializing the value be 1
 if number >= 0
 {
-    if(number != 0)
+    if(number != 0)                // checks the condition
     {
         for _ in 0...number-1
         {
-            value=value*2
+            value=value*2        //calculation of Power of 2
         }
         print("2^\(number) : \(value)")
     }
