2033705b45672ab835c6a2943b141c00872d4e32
Viswa527 <viswarupaachari88@gmail.com>
2020-03-05 10:33:13 +0530

    [Viswa]Refactor.added comments LeapYear

--- LeapYear/main.swift
+++ LeapYear/main.swift
@@ -5,11 +5,10 @@
 //  Created by admin on 05/03/20.
 //  Copyright © 2020 admin. All rights reserved.
 //
-
 import Foundation
-var num=Int(readLine()!)!
+var num=Int(readLine()!)!    // User input
 
-if num%400==0 || num%4==0 && num%100 != 0
+if num%400==0 || num%4==0 && num%100 != 0    //condition to check Number is leapyear or not
 {
     print("\(num) is a Leap Year")
 }
