71895671eb21d6f2e0bba6b00545a675753e8fa2
Viswa527 <viswarupaachari88@gmail.com>
2020-03-19 16:42:01 +0530

    [Viswa]Refactor.values are estimated with respective of items in inventory

--- JSON InventoryDataManagement/main.swift
+++ JSON InventoryDataManagement/main.swift
@@ -7,19 +7,24 @@
 //
 
 import Foundation
-
-let objItems = Items()
-let path = "/Users/admin/Desktop/Swift/ObjectOrientedProblems/JSON InventoryDataManagement/file.json"
+let path = "/Users/admin/Desktop/Swift/ObjectOrientedProblems/JSON InventoryDataManagement/files.json"
 var myValues:String!
 do {
     let data = try NSString(contentsOfFile: path,encoding: String.Encoding.ascii.rawValue)
     myValues = data as String
 }
+var json:[String: [String:Any]]?
+var values:[String] = []
+let properties = ["Name","AvailablityOfWeight","PricePerKg"]
 let data = Data(myValues.utf8)
 do {
-    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+    if let jsonn = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
     {
-        print(json)
+        json = (jsonn as! [String : [String : Any]])
+        for i in jsonn.keys
+        {
+            values.append(i)
+        }
     }
 }
 catch let error as NSError
@@ -27,3 +32,21 @@ catch let error as NSError
     print("Failed to load: \(error.localizedDescription)")
 
 }
+var inventoryAmount = 0
+for i in values
+{
+    var amount = 1
+     print(json![i]!["Name"]!)
+    for j in properties
+    {
+        if j != "Name"
+        {
+            amount = amount * (json![i]![j]! as! Int)
+            print("\(j)  : \(json![i]![j]!) ")
+        }
+    }
+    inventoryAmount = inventoryAmount + amount
+    print("Total amount of \(i) get by selling : \(amount)")
+    print("-----------------------------------------------")
+}
+print("Total assert in inventory is : \(inventoryAmount)")
