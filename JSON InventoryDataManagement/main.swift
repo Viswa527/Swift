de576df95691e9b5148baf532c6edc4a29607da3
Viswa527 <viswarupaachari88@gmail.com>
2020-03-19 14:58:38 +0530

    [Viswa]Added JSON InventoryDataManagement program

--- JSON InventoryDataManagement/main.swift
+++ JSON InventoryDataManagement/main.swift
@@ -1,30 +1,25 @@
 //
 //  main.swift
-//  ObjectOrientedProblems
+//  JSON InventoryDataManagement
 //
-//  Created by admin on 13/03/20.
+//  Created by admin on 19/03/20.
 //  Copyright © 2020 admin. All rights reserved.
 //
 
 import Foundation
 
-let path = "/Users/admin/Desktop/Swift/ObjectOrientedProblems/AddressBook/file.json"
+let objItems = Items()
+let path = "/Users/admin/Desktop/Swift/ObjectOrientedProblems/JSON InventoryDataManagement/file.json"
 var myValues:String!
 do {
     let data = try NSString(contentsOfFile: path,encoding: String.Encoding.ascii.rawValue)
     myValues = data as String
 }
 let data = Data(myValues.utf8)
-var jsonn:Any?
-let items = ["Rice","Wheats","Pulse"]
-
 do {
     if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
     {
-        for nitems in items
-        {
-            print(json[nitems]!)
-        }
+        print(json)
     }
 }
 catch let error as NSError
@@ -32,4 +27,3 @@ catch let error as NSError
     print("Failed to load: \(error.localizedDescription)")
 
 }
-
